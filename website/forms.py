c6c9011c146e245a9f3693361a93cd81db822b7a
Alex Steeve <alexsteeve@gmail.com>
2018-09-06 19:21:44 +0000

    implantacao

---
+++ website/forms.py
@@ -0,0 +1,24 @@
+from helloworld.models import Estudante
+from django import forms
+
+# FORMULÁRIO DE INCLUSÃO DE ESTUDANTES
+# -------------------------------------------
+
+class InsereEstudanteForm(forms.ModelForm):
+
+    class Meta:
+        # Modelo base
+        model = Estudante
+
+        # Campos que estarão no form
+        fields = [
+            'nome',
+            'matricula',
+            'email',
+            'endereco',
+            'cpf',
+            'celular',
+            'dataNascimento',
+            'batismo',
+            'escolaridade'
+        ]
